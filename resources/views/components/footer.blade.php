e43820ef81047a2b44f6bdf808a757de4bb0f7f4
Davis SHYAKA <shyakakingdavis@gmail.com>
2022-11-08 09:05:35 +0200

    Initial Commit

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,16 @@
+<footer class="flex-shrink-0 px-6 py-4">
+    <p class="flex items-center justify-center gap-1 text-sm text-gray-600 dark:text-gray-400">
+        <span>Created</span>
+        {{-- <span>
+            <x-heroicon-s-heart class="w-6 h-6 text-red-500" />
+        </span> --}}
+        <span>by</span>
+        <a href="https://github.com/davis-shyaka" target="_blank" class="text-blue-600 hover:underline">
+            SHYAKA King Davis
+        </a>
+        <span>&</span>
+        <a href="https://github.com/davis-shyaka" target="_blank" class="text-blue-600 hover:underline">
+            HAFASHIMANA Germain
+        </a>
+    </p>
+</footer>
