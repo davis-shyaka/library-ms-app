4b7178e651576a297e9b80c5b50c37400e99bce9
Davis SHYAKA <shyakakingdavis@gmail.com>
2022-11-21 22:48:04 +0200

    move search bar into the table and align buttons

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -8,25 +8,7 @@
     </x-slot>
 
 
-    <div class="flex justify-center mb-2">
-        <div class="relative mt-2">
-            <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor"
-                    viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd"
-                        d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                        clip-rule="evenodd"></path>
-                </svg>
-            </div>
-            <form method="POST" action="{{ route('users.search') }}">
-                @csrf
-                <input type="text" id="table-search" name='item'
-                    class="block p-2 pl-10 w-80 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Search for users" />
-            </form>
 
-        </div>
-    </div>
     <div class="">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="overflow-hidden shadow-sm sm:rounded-lg">
@@ -35,9 +17,28 @@
                     <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8 dark:bg-[#020917] p-4">
                         <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                             <div class="shadow overflow-hidden border-b border-cyan-400 sm:rounded-lg">
-                                <div>
-                                    <a class="block p-2 mb-4 w-fit text-sm text-gray-900 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 bg-green-500 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                        href="{{ route('users.create') }}">Add new staff</a>
+                                <div class="flex justify-center mb-2">
+                                    <div class="relative mt-2">
+                                        <div
+                                            class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                                            <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                                                fill="currentColor" viewBox="0 0 20 20"
+                                                xmlns="http://www.w3.org/2000/svg">
+                                                <path fill-rule="evenodd"
+                                                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                                                    clip-rule="evenodd"></path>
+                                            </svg>
+                                        </div>
+                                        <form method="POST" action="{{ route('users.search') }}">
+                                            @csrf
+                                            <input type="text" id="table-search" name='item'
+                                                class="block p-2 pl-10 w-80 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                                placeholder="Search for users" />
+                                        </form>
+
+                                    </div>
+                                </div>
+                                <div class="flex justify-between mx-10">
                                     <div class="flex flex-row gap-4">
                                         <a class="block p-2 mb-4 w-fit text-sm text-black rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                             href="{{ route('users.index') }}">All</a>
@@ -54,6 +55,9 @@
                                                 Directors</button>
                                         </form>
                                     </div>
+
+                                    <a class="block p-2 mb-4 w-fit text-sm text-gray-900 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 bg-green-500 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        href="{{ route('users.create') }}">Add new staff</a>
                                 </div>
 
                                 <table class="min-w-full divide-y divide-gray-200">
